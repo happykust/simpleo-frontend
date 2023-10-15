bf7a21bdc01bdd410595c6794a2cb3e13aaba1e5
Kirill Nikolaevskiy <happykust@cyberzone.dev>
2023-10-15 18:15:40 +0300

    Add copyright

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
+<!--
+  - Copyright (C) Cyberzone MIREA, Inc - All Rights Reserved
+  - Unauthorized copying of this file, via any medium is strictly prohibited
+  - Proprietary and confidential
+  -->
+
 <template>
   <div>
     <NuxtWelcome />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) Cyberzone MIREA, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true }
