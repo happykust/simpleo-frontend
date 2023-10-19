fe81601bd8f794806cfe82e280d47bea15ab0e50
Kirill Nikolaevskiy <happykust@cyberzone.dev>
2023-10-19 20:48:24 +0300

    New scanner

--- pages/bookings/user/scan.vue
+++ pages/bookings/user/scan.vue
@@ -10,12 +10,18 @@
     <button type="button" class="border border-green-500 bg-green-700 text-white rounded p-2.5 mb-2">Назад</button>
   </NuxtLink>
   <h1 class="text-2xl font-bold mb-3">{{ status }}</h1>
-  <div id="qrcode" class="w-72"></div>
+  <StreamBarcodeReader class="w-72" @decode="onDecode"></StreamBarcodeReader>
 </template>
+
+
+<script setup>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+</script>
 
 <script>
 
 import { ref } from "vue";
+import { StreamBarcodeReader } from "vue-barcode-reader";
 import useAuthedFetch from "~/addons/useAuthedFetch";
 
 const status = ref("Отсканируйте QR код");
@@ -27,15 +33,8 @@ export default {
 
     return {decodedText, status}
   },
-  mounted() {
-    const html5QrcodeScanner = new Html5QrcodeScanner('qrcode', {
-      rememberLastUsedCamera: false,
-      supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
-    });
-    html5QrcodeScanner.render(this.onDecode);
-  },
   methods: {
-    async onDecode(text, _) {
+    async onDecode(text) {
       this.status = "Рассматриваем баги..."
       this.status = await useAuthedFetch("/booking/qr", {
         method: "POST",
